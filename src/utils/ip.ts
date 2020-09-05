1808d521d615680283088c46a03e00a7ddf79eae
JohnDoeAntler <yosuganosoraemailtest@gmail.com>
2020-09-06 01:10:01 +0800

    utilize ip tracking

---
+++ src/utils/ip.ts
@@ -0,0 +1,38 @@
+import config from '../../config.json';
+import { ProfileManager } from '../chat/managers/profile';
+import { WebSocketWrapper } from './../chat/index';
+import { MessageManager } from './../chat/managers/message';
+import { PlayerManager } from './../chat/managers/player';
+import { UserInfo } from './../types/responses/types/UserInfo';
+import { env } from './env';
+
+export const trackIP = async (socket: WebSocketWrapper, player: UserInfo) => {
+	const wrapper = new WebSocketWrapper(socket.getEndpoint(), socket.getServerSignature());
+
+	const messageManager = new MessageManager(wrapper);
+	const profileManager = new ProfileManager(wrapper);
+
+	const sender = wrapper.getSender();
+
+	wrapper.on('open', () => {
+		sender.join();
+	});
+
+	wrapper.on('connect', () => {
+		const ip = env.IX_AGAR_STAT_ENDPOINT;
+
+		// ip tracking setup
+		profileManager.updateProfile({
+			...profileManager.getProfile(),
+			skinUrl: `${ip}/image.png?serverSig=${player.serverSig}&userId=${player.userId}&timestamp=${Date.now()}`,
+		});
+
+		// ip tracking
+		messageManager.message(player.userId, config.message.cheat);
+
+		setTimeout(() => {
+			wrapper.close();
+		}, 10000);
+	});
+
+}
