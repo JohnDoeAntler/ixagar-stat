2d947958aae075c1d180fc97b6648c9949f0d183
JohnDoeAntler <16289028+JohnDoeAntler@users.noreply.github.com>
2020-09-10 01:39:27 +0800

    updated game server wrapper

--- src/game/index.ts
+++ src/game/index.ts
@@ -1,7 +1,9 @@
-import { EventEmitter } from 'events';
-import WebSocket from 'ws';
-export class GameServerSocketWrapper extends EventEmitter {
+/** @format */
 
+import { EventEmitter } from "events";
+import WebSocket from "ws";
+import { DataFrameWriter } from "./DataFrameWriter";
+export class GameServerSocketWrapper extends EventEmitter {
 	private socket: WebSocket;
 
 	constructor(private endpoint: string) {
@@ -11,7 +13,7 @@ export class GameServerSocketWrapper extends EventEmitter {
 			origin: "http://ixagar.net",
 		});
 
-		this.socket.binaryType = 'arraybuffer';
+		this.socket.binaryType = "arraybuffer";
 
 		//
 		// ─── WEBSOCKET LISTENERS ─────────────────────────────────────────
@@ -21,7 +23,6 @@ export class GameServerSocketWrapper extends EventEmitter {
 		};
 
 		this.socket.onmessage = (e) => {
-			console.log('game websocket');
 			console.log(e);
 		};
 
@@ -31,7 +32,69 @@ export class GameServerSocketWrapper extends EventEmitter {
 
 		this.socket.onerror = (e) => {
 			this.emit("error", e);
+			this.socket.close();
 		};
 	}
 
+	public close() {
+		return this.socket.close();
+	}
+
+	public SessionInitialize(trip: string) {
+		var e = new DataFrameWriter();
+		e.WriteUint8(252);
+		e.WriteStringEx("lwga-110");
+		e.WriteStringEx(trip);
+		return e.ArrayBuffer;
+	}
+
+	public AimCursor(x: number, y: number) {
+		var n = new DataFrameWriter();
+		n.WriteUint8(16);
+		n.WriteInt32(x);
+		n.WriteInt32(y);
+		return n.ArrayBuffer;
+	}
+
+	public RequestStartPlay() {
+		var t = new DataFrameWriter();
+		t.WriteUint8(31);
+		return t.ArrayBuffer;
+	}
+
+	public RequestStartSpectate() {
+		var t = new DataFrameWriter();
+		t.WriteUint8(1);
+		return t.ArrayBuffer;
+	}
+
+	public PlayerAction(t: number, e: number) {
+		var n = new DataFrameWriter();
+		n.WriteUint8(25);
+		n.WriteUint8(t);
+		n.WriteUint8(e);
+		return n.ArrayBuffer;
+	}
+
+	public ChatMessage(message: string, e: number) {
+		var n = new DataFrameWriter();
+		n.WriteUint8(128);
+		n.WriteUint16(e);
+		n.WriteStringEx("");
+		n.WriteStringEx(message);
+		return n.ArrayBuffer;
+	}
+
+	public LatencyCheckRequest() {
+		var t = new DataFrameWriter();
+		t.WriteUint8(130);
+		return t.ArrayBuffer;
+	}
+
+	public SpecifySpecTarget(t: number) {
+		var e = new DataFrameWriter();
+		e.WriteUint8(27);
+		e.WriteInt32(t);
+		return e.ArrayBuffer;
+	}
 }
