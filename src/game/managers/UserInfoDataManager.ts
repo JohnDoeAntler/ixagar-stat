c58b7d330030b0cf6d75ea0a844458a6d45719ea
JohnDoeAntler <16289028+JohnDoeAntler@users.noreply.github.com>
2020-09-11 03:49:10 +0800

    added user info data manager

---
+++ src/game/managers/UserInfoDataManager.ts
@@ -0,0 +1,91 @@
+import { MessageEvent } from 'ws';
+import { MapUserInfoData } from '../../types/game/MapUserInfoData';
+import { UserInfoData } from '../../types/game/UserInfoData';
+import { DataFrameReader } from '../DataFrameReader';
+import { GameServerSocketWrapper } from './../index';
+
+export class UserInfoDataManager {
+
+	private users = new Map<number, UserInfoData>();
+
+	private map = new Map<number, MapUserInfoData>();
+
+	constructor(socket: GameServerSocketWrapper) {
+		socket.on('message', (e: MessageEvent) => {
+			const test = e.data;
+
+			var i = new DataFrameReader(test);
+
+			switch (i.ReadUint8()) {
+				case 42: 
+					for (let f = 0; f < i.ReadUint16(); f++) {
+						// num
+						const num = i.ReadUint16();
+
+						// model
+						const user = {
+							num,
+							name: i.ReadStringEx(),
+							team: i.ReadStringEx(),
+							skinUrl1: i.ReadStringEx(),
+							isBot: i.ReadUint8() > 0,
+							teamId: i.ReadUint16(),
+							trip: i.ReadStringEx(),
+							skinUrl2: i.ReadStringEx(),
+						};
+
+						// event
+						if (this.users.has(num)) {
+							socket.emit('change', this.users.get(num), user);
+						} else {
+							socket.emit('join', user);
+						}
+
+						// map user by num
+						this.users.set(num, user);
+					}
+					break;
+				case 45:
+					for (let f = 0; f < i.ReadUint16(); f++) {
+						const num = i.ReadUint16();
+
+						// event
+						socket.emit('leave', this.users.get(num));
+
+						// update map
+						this.users.delete(num);
+					}
+					break;
+				case 41:
+					this.map.clear();
+
+					for (let f = 0; f < i.ReadUint16(); f++) {
+						const playerId = i.ReadUint16();
+						const x = i.ReadInt16();
+						const y = i.ReadInt16();
+						const mass = i.ReadUint16();
+
+						this.map.set(playerId, {
+							user: this.users.get(playerId),
+							x,
+							y,
+							mass,
+						});
+					}
+					break;
+			}
+		});
+	}
+
+	//
+	// ─── GETTER ─────────────────────────────────────────────────────────────────────
+	//
+	public getUsers() {
+		return this.users;
+	}
+
+	public getMap() {
+		return this.map;
+	}
+
+}
