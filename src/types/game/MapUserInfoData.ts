5f33b8480b0e99b1effa3568671374e588e3a186
JohnDoeAntler <16289028+JohnDoeAntler@users.noreply.github.com>
2020-09-11 03:36:04 +0800

    added game server typings

---
+++ src/types/game/MapUserInfoData.ts
@@ -0,0 +1,8 @@
+import { UserInfoData } from "./UserInfoData";
+
+export interface MapUserInfoData {
+	user: UserInfoData;
+	x: number;
+	y: number;
+	mass: number;
+}
---
+++ src/types/game/UserInfoData.ts
@@ -0,0 +1,10 @@
+export interface UserInfoData {
+	num: number;
+	name: string;
+	team: string;
+	skinUrl1: string;
+	isBot: boolean;
+	teamId: number;
+	trip: string;
+	skinUrl2: string;
+}
